416bf8a4c48a05bf51bf867ebb85bc89035a40b9
Oleg <serv_all@mail.ru>
2023-08-11 12:21:32 +0300

    Добавлены классы для сериализации-десериализации заказов в БД

---
+++ OrderProcessing/Entityes/JSONModels/OrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderProcessing
+{
+    public class OrderDTO
+    {
+        public string OrderNumber { get; set; }
+        public List<Product> Products { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}

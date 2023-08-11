c151d5b1e778393edaa8a9062dd87ce27820c646
Oleg <serv_all@mail.ru>
2023-08-12 00:42:31 +0300

    Введены атрибуты для корректной сериализации-десериализации

---
+++ OrderProcessing/Entityes/JSONModels/OrderJSON.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OrderProcessing
+{
+    public class OrderJSON
+    {
+        [JsonPropertyName("orderNumber")]
+        public string OrderNumber { get; set; }
+        [JsonPropertyName("products")]
+        public List<Product> Products { get; set; }
+        [JsonPropertyName("createdAt")]
+        public string CreatedAt { get; set; }
+    }
+}
--- OrderProcessing/Entityes/JSONModels/Product.cs
+++ OrderProcessing/Entityes/JSONModels/Product.cs
@@ -1,16 +1,28 @@
-﻿namespace OrderProcessing
+﻿using System.Text.Json.Serialization;
+
+namespace OrderProcessing
 {
     public class Product
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("comment")]
         public string Comment { get; set; }
+        [JsonPropertyName("quantity")]
         public string Quantity { get; set; }
+        [JsonPropertyName("paidPrice")]
         public string PaidPrice { get; set; }
+        [JsonPropertyName("unitPrice")]
         public string UnitPrice { get; set; }
+        [JsonPropertyName("remoteCode")]
         public string RemoteCode { get; set; }
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+        [JsonPropertyName("vatPercentage")]
         public string VatPercentage { get; set; }
+        [JsonPropertyName("discountAmount")]
         public string DiscountAmount { get; set; }
     }
 }

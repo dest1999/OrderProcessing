8e39aa6bf73a09f148036edf5fd1c91a365e9737
Oleg <serv_all@mail.ru>
2023-08-13 21:32:48 +0300

    Добавление миграции для БД

--- OrderProcessing/Entityes/JSONModels/OrderDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderProcessing
-{
-    public class OrderDTO
-    {
-        public string OrderNumber { get; set; }
-        public List<Product> Products { get; set; }
-        public string CreatedAt { get; set; }
-    }
-}

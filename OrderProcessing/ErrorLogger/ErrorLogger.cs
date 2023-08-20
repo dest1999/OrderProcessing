1a281964dd69ed8c64f07e41227d34c6e1752d5d
Oleg <serv_all@mail.ru>
2023-08-20 17:20:55 +0300

    В лог записывается количество возникших при обработке ошибок

--- OrderProcessing/ErrorLogger/ErrorLogger.cs
+++ OrderProcessing/ErrorLogger/ErrorLogger.cs
@@ -5,6 +5,7 @@ namespace OrderProcessing
     public class ErrorLogger : IErrorLogger
     {
         private ConcurrentQueue<string> _errors = new();
+        private int _errorCount = 0;
         public void WriteToLog(string message)
         {
             _errors.Enqueue(message);
@@ -20,7 +21,7 @@ namespace OrderProcessing
                     Task.Run(() =>
                     {
                         Thread.Sleep(10_000);//возможно задание в части задержки записи ошибки в лог понято некорректно
-                        File.AppendAllText("ErrorMessages.log", $"Writing at {DateTime.Now} message:\n{message}\n");
+                        File.AppendAllText("ErrorMessages.log", $"Writing at {DateTime.Now} error message # {++_errorCount}:\n{message}\n");
                         Thread.Sleep(10_000);
                     });
 
